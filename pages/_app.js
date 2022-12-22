cdeb72760ed3dcf54979c9ad7f185a06539b497c
Nazmul <Nazmul.abbu@gmail.com>
2022-12-23 02:33:58 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import * as React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? "light" : "light",
+                },
+                typography: {
+                    fontFamily: "Open Sans",
+                },
+            }),
+        [prefersDarkMode]
+    );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
